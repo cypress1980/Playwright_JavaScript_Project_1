38f74c667b9089fc8dabefb8af775c14a411d40f
Kailash Pathak <kailash.pathak@3pillarglobal.com>
2022-09-11 15:36:47 +0530

    new tc added

--- tests/qaautomationlabs.spec.js
+++ tests/qaautomationlabs.spec.js
@@ -19,3 +19,20 @@ test("Serach the Blog with text 'Playwright  ", async ({ page }) => {
   await search.type("Playwright");
   await searchIcon.click();
 });
+test.only("Login into https://www.saucedemo.com/ and logout  ", async ({
+  page,
+}) => {
+  await page.goto("https://www.saucedemo.com/");
+  const userName = page.locator("id=user-name");
+  const password = page.locator("id=password");
+  const submitButton = page.locator("id=login-button");
+  const humBurger = page.locator("id=react-burger-menu-btn");
+  const logout = page.locator("id=logout_sidebar_link");
+  await userName.click();
+  await userName.type("standard_user");
+  await password.click();
+  await password.type("secret_sauce");
+  await submitButton.click();
+  await humBurger.click();
+  await logout.click();
+});
